018b889239997c7aac1e696ac5293aff59f2842b
frankkator <frankkkator05gmail.com>
2026-08-26 12:35:31 +0100

    changes

--- src/Components/Container/WelcomePage.tsx
+++ src/Components/Container/WelcomePage.tsx
@@ -55,7 +55,7 @@ export default function WelcomePage() {
       
       {/* Floating Header */}
       <div className='flex justify-center border border-amber-200/20 rounded-2xl shadow-md p-4 bg-white/10 backdrop-blur-sm mb-6'>
-        <h1 className="font-custom font-extrabold text-4xl text-amber-950">
+        <h1 className="font-custom font-extrabold text-4xl  text-amber-950">
           Kator's Portfolio
         </h1>
       </div>
@@ -77,12 +77,12 @@ export default function WelcomePage() {
         {/* Right Column: Profile Content Box */}
         <div className="lg:grid-span-1">
         <div className=' rounded-3xl p-6 shadow-lg min-62.5 border border-amber-200 bg-white/30 backdrop-blur-md'>
-          <p className="font-custom text-black text-4xl font-extrabold"> 
+          <p className="font-custom text-black text-4xl text-center font-extrabold"> 
               Hi there, So my Name is Franklin Kator i'm a software developer and web-designer. I build mordern, interactive digital experinces and I'd love to work with you.
           </p>      
         </div>
         <div className='rounded-3xl p-6 shadow-lg min-62.5 border mt-6 border-amber-200 bg-white/30 '>
-          <p  className="font-custom text-black text-3xl font-extrabold">
+          <p  className="font-custom text-black text-3xl text-center font-extrabold">
               Soo, kindly, check out the projects and prompts i've done and contact me if interested.
           </p>
           </div>
